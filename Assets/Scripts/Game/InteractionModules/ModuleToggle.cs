64bcdd13d5dea488c12317061bd242bd4f2ef6f0
errico6 <125646224+errico6@users.noreply.github.com>
2025-12-07 18:35:30 +0100

    UI repaired
    Co-Authored-By: Roger Puchol Celma <49079760+Ropuce@users.noreply.github.com>
    Co-Authored-By: Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>
    Co-Authored-By: Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>

---
+++ Assets/Scripts/Game/InteractionModules/ModuleToggle.cs
@@ -0,0 +1,23 @@
+using Game;
+using Game.InteractionModules;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModuleToggle : InteractableModule
+{
+    [SerializeField] private Toggle toggle;
+
+    private void Start()
+    {
+        _moduleData.moduleName = "ModuleToggle";
+        _moduleData.ValueTypes = SentDataTypes.Bool;
+
+        toggle.onValueChanged.AddListener(OnToggleChange);
+    }
+
+    private void OnToggleChange(bool value)
+    {
+        _moduleData.boolValue = value;
+        SendMessageToManager();
+    }
+}
--- Assets/Scripts/Game/UIManagerEnviador.cs
+++ Assets/Scripts/Game/UIManagerEnviador.cs
@@ -1,74 +1,70 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class UIManagerEnviador : MonoBehaviour
 {
-    public Slider puertaSlider;
-    public Toggle bloqueoToggle;
-    public Button accion1Button;
-    public Button accion2Button;
     private ClientManager clientManager;
     private ServerManager serverManager;
-    private UIManagerReceptor localReceptor;
-    public void EnviarEstadoUI()
-    {
-        UIStateMessage uiState = new UIStateMessage();
 
-        uiState.sliderPuertaValue = puertaSlider.value;
-        uiState.toggleBloqueoValue = bloqueoToggle.isOn;
-        uiState.accion1Interactable = accion1Button.interactable;
-        uiState.accion2Interactable = accion2Button.interactable;
-
-        string dataJson = JsonUtility.ToJson(uiState);
-
-        NetMessage msgParaEnviar = new NetMessage("UI_Update", dataJson);
-
-        EnviarMensaje(msgParaEnviar);
-
-        string jsonFinal = JsonUtility.ToJson(msgParaEnviar);
-
-        Debug.Log("Enviando estado: " + jsonFinal);
-    }
-
+    private List<Slider> sliders = new();
+    private List<Toggle> toggles = new();
+    private List<Button> buttons = new();
 
     void Start()
     {
-        puertaSlider.onValueChanged.AddListener((valor) => EnviarEstadoUI());
-        bloqueoToggle.onValueChanged.AddListener((activo) => EnviarEstadoUI());
-
-        accion1Button.onClick.AddListener(() => EnviarAccion1());
-
         clientManager = FindObjectOfType<ClientManager>();
         serverManager = FindObjectOfType<ServerManager>();
 
-        if (serverManager != null)
-        {
-            localReceptor = FindObjectOfType<UIManagerReceptor>();
-        }
+        sliders.AddRange(FindObjectsOfType<Slider>());
+        toggles.AddRange(FindObjectsOfType<Toggle>());
+        buttons.AddRange(FindObjectsOfType<Button>());
+
+        foreach (var s in sliders)
+            s.onValueChanged.AddListener((_) => EnviarEstadoUI());
+
+        foreach (var t in toggles)
+            t.onValueChanged.AddListener((bool activo) =>
+            {
+                EnviarEstadoUI();
+                Debug.Log($"Toggle {t.name} está ahora {(activo ? "ON" : "OFF")}");
+            });
+
+        foreach (var b in buttons)
+            b.onClick.AddListener(() => EnviarAccion(b));
     }
 
-    public void EnviarAccion1()
+    public void EnviarEstadoUI()
     {
-        NetMessage msgParaEnviar = new NetMessage("Accion1_Clicked", "");
-        EnviarMensaje(msgParaEnviar);
-        Debug.Log("Enviando Accion 1");
+        UIStateMessage state = new UIStateMessage();
+
+        foreach (var s in sliders)
+            state.sliders[s.name] = s.value;
+
+        foreach (var t in toggles)
+            state.toggles[t.name] = t.isOn;
+
+        foreach (var b in buttons)
+            state.buttons[b.name] = b.interactable;
+
+        string json = JsonUtility.ToJson(state);
+        EnviarMensaje(new NetMessage("UI_Update", json));
+
+        Debug.Log("Estado UI enviado.");
     }
+
+    public void EnviarAccion(Button b)
+    {
+        EnviarMensaje(new NetMessage("Button_Clicked", b.name));
+        Debug.Log($"Enviando acción: {b.name}");
+    }
+
 
     private void EnviarMensaje(NetMessage msg)
     {
         if (clientManager != null)
-        {
             clientManager.SendMessageToServer(msg);
-        }
-        else if (serverManager != null)
-        {
+        else
             serverManager.BroadcastMessage(msg);
-
-            string jsonMsg = JsonUtility.ToJson(msg);
-            if (localReceptor != null)
-            {
-                localReceptor.RecibirMensaje(jsonMsg);
-            }
-        }
     }
 }
